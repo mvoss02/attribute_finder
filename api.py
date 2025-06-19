04856156611cee4044e09e2b342c0626bafad7a4
mvoss02 <m_l_voss@web.de>
2025-06-19 13:38:16 +0200

    Added a status endpoint

--- api.py
+++ api.py
@@ -3,7 +3,9 @@ import asyncio
 from run import main
 from config.config import data_config
 
+
 app = FastAPI()
+is_running = False  # shared app-level state
 
 @app.get("/")
 def root():
@@ -11,5 +13,25 @@ def root():
 
 @app.post("/start-processing")
 async def start_processing():
-    asyncio.create_task(main(batch_size=data_config.batch_size))
+    global is_running
+
+    if is_running:
+        return {"status": "already running"}
+
+    is_running = True
+
+    async def wrapped_main():
+        global is_running
+        try:
+            await main(batch_size=data_config.batch_size)
+        finally:
+            is_running = False
+
+    asyncio.create_task(wrapped_main())
+
     return {"status": "processing started"}
+
+@app.get("/status")
+def status():
+    return {"running": is_running}
+
